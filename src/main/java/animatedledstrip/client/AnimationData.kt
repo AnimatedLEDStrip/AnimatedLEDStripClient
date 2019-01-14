3129e96a5d42267dcf9ffe1980f5b5330d3c9358
Max Narvaez <narvae1@stolaf.edu>
2019-01-14 00:07:46 -0600

    Add copyright

--- src/main/java/animatedledstrip/client/AnimationData.kt
+++ src/main/java/animatedledstrip/client/AnimationData.kt
@@ -1,6 +1,29 @@
 package animatedledstrip.client
 
-import animatedledstrip.leds.Animations
+/*
+ *  Copyright (c) 2019 AnimatedLEDStrip
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+
+import animatedledstrip.leds.Animation
 import animatedledstrip.leds.Direction
 import org.pmw.tinylog.Logger
 import java.lang.IllegalArgumentException
@@ -17,13 +40,13 @@ class AnimationData {
     /*
      * Parameters and functions to create animation map
      */
-    var animation: Animations
+    var animation: Animation
         set(value) {
             animationMap["Animation"] = value
         }
-        get() = animationMap["Animation"] as Animations? ?: throw UninitializedPropertyAccessException()
+        get() = animationMap["Animation"] as Animation? ?: throw UninitializedPropertyAccessException()
 
-    fun animation(animation: Animations): AnimationData {
+    fun animation(animation: Animation): AnimationData {
         this.animation = animation
         return this
     }
