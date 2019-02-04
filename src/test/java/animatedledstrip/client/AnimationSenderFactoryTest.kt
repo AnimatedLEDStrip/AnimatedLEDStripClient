6fda7602abf68374bb54ee50d199bfe5ea54f553
Max Narvaez <narvae1@stolaf.edu>
2019-02-04 17:41:49 -0600

    Add test for sender start

--- src/test/java/animatedledstrip/client/AnimationSenderFactoryTest.kt
+++ src/test/java/animatedledstrip/client/AnimationSenderFactoryTest.kt
@@ -1,6 +1,11 @@
 package animatedledstrip.client
 
+import kotlinx.coroutines.*
 import org.junit.Test
+import java.io.BufferedInputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.net.ServerSocket
 import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
@@ -15,6 +20,25 @@ class AnimationSenderFactoryTest {
 
         val testSender = AnimationSenderFactory.create("localhost", 5).setAsDefaultSender()
         assertTrue { AnimationSenderFactory.defaultSender === testSender }
+    }
+
+    @Test
+    fun testStart() {
+        GlobalScope.launch {
+            withContext(Dispatchers.IO) {
+                val socket = ServerSocket(5).accept()
+                val socIn = ObjectInputStream(BufferedInputStream(socket!!.getInputStream()))
+
+                val input: Any? = socIn.readObject()
+                val inMap = input as Map<*, *>
+                assertTrue { inMap["ClientData"] == true }
+                assertTrue { inMap["TextBased"] == false }
+            }
+        }
+
+        runBlocking { delay(2000) }
+
+        AnimationSenderFactory.create("localhost", 5).start()
     }
 
 }
