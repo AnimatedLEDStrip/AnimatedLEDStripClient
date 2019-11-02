d95452b51b46d286ec0525873168305c7419a919
Max Narvaez <mnmax.narvaez3@gmail.com>
2019-11-01 19:45:42 -0500

    Add test.cpp

---
+++ client-c++/test.cpp
@@ -0,0 +1,32 @@
+//
+// Created by mnmax on 11/1/2019.
+//
+
+#include "AnimationSender.h"
+
+int main() {
+
+    AnimationSender sender("10.44.157.2", 5);
+
+    sender.start();
+
+    sleep(10);
+
+    AnimationData d;
+    ColorContainer c;
+    c.addColor(0xFF);
+    c.addColor(0xFF00);
+    d.animation = BOUNCE;
+    d.addColor(c);
+    sender.sendAnimation(d);
+
+    sleep(200);
+    sender.end();
+
+    for (std::pair<std::string, AnimationData *> a : *sender.running_animations) {
+        char *b = new char[1000];
+        a.second->json(&b);
+        printf("%s=%s\n", a.first.c_str(), b);
+    }
+
+}
